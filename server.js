60e77b3c9e87bd25f3f5175ddf69f4503d6a4a19
lam1ang <63092113+sillygiang2f5@users.noreply.github.com>
2021-11-25 14:57:42 +0700

    :)

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+const { connect } = require('http2');
 const io = require('socket.io')(3000)
 
-var player=[]
+var connectedPlayers=[];
+var adminID="";
 
 io.on('connection', socket => {
   console.log(socket.id);
-  socket.broadcast.emit(socket.id,"Connected"); //?
-})
+  socket.on('init-authenticate', (authID,callback) =>{
+    if(fs.existsSync(`users_data/${authID}.json`)){
+      var player=JSON.parse(`users_data/${authID}.json`);
+      callback({
+        ifValid:true,
+        playerInfo:player
+      })
+      connectedPlayers.push(authID);
+      socket.to(adminID).emit('update-connected-players', connectedPlayers);
+    }else if(authID=="BTC"){
+      adminID=socket.id;
+      callback({
+        ifValid:true,
+        _connectedPlayers:connectedPlayers
+      })
+    }
+  })
 
-io.on('connect',{})
-io.on('init-authenticate', (authID,callback) =>{
-  if(fs.existsSync(`users_data/${authID}.json`)){
-    var player=JSON.parse(`users_data/${authID}.json`);
-    callback({
-      ifValid:true,
-      playerInfo:player
-
-    }) 
-  }else if(authID="BTC"){
-    callback({
-      ifValid:true
-    })
-  }
 })
 
