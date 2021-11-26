b0ffc19a277b128c08c34ff908ef8ca4a7438dd3
Maik <maikpla3@gmail.com>
2021-11-26 21:31:50 +0700

    Sửa lỗi + Thêm function để redirect thí sinh
    Sửa một số lỗi:
     - Đọc file JSON sai
     - Thiếu CORS
     - Cập nhật thí sinh khi kết nối, ngắt kết nối
     Thêm:
     - Function redirect thí sinh

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
 const fs = require('fs');
 const { connect } = require('http2');
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+  cors: {
+    origin: 'http://localhost:4200',
+  }
+});
 
-var connectedPlayers={};
+var connectedPlayers = [];
+var socketIDs = [];
 var adminID="";
-
+var playerCount = 0;
 io.on('connection', socket => {
   console.log(socket.id);
   socket.on('init-authenticate', (authID,callback) =>{
     if(fs.existsSync(`users_data/${authID}.json`)){
-      var player=JSON.parse(`users_data/${authID}.json`);
+      var player=JSON.parse(fs.readFileSync(`users_data/${authID}.json`));
       callback({
         ifValid:true,
-        playerInfo:player
+        playerInfo:player,
       })
-      connectedPlayers[socket.id]=authID;
+      connectedPlayers.push(player);
+      socketIDs.push(socket.id);
+      console.log(connectedPlayers);
+      console.log(socketIDs);
+     // console.log('Số người chơi: ', playerCount)
+     // console.log("Người chơi số " + playerCount + " là " + player.name);
       socket.to(adminID).emit('update-connected-players', connectedPlayers);
     }else if(authID=="BTC"){
       adminID=socket.id;
       callback({
         ifValid:true,
-        _connectedPlayers:connectedPlayers
+        connectedPlayers:connectedPlayers
       })
     }
   })
-  socket.on('disconnect', ()=>{
-      delete connectedPlayers[socket.id];
+  socket.on('disconnect', () => {
+    console.log('disconnect');
+    connectedPlayers.splice(connectedPlayers.indexOf(socketIDs[socket.id]), 1);
+    socketIDs.splice(socketIDs.indexOf(socket.id), 1);
+    console.log(connectedPlayers)
+    socket.to(adminID).emit('update-connected-players', connectedPlayers);
   })
-
+  socket.on('beginMatch', () => {
+    console.log('Bắt đầu phần khởi động');
+    socket.broadcast.emit('beginMatch');
+  })
 })
 
