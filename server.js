30990e2c5556b185a603e550d5504bb32f31dd2d
Maik <maikpla3@gmail.com>
2022-02-14 12:47:23 +0700

    Fixed found bugs, implemented importing data from Excel and added 2 more question types to Về Đích

--- server.js
+++ server.js
@@ -28,20 +28,21 @@ var lastTurnId = '';
 var timerActive = false;
 var ifLastAnswerCorrect = false;
 var ifFiveSecActive = false;
+let mainTimer = 0;
 var threeSecTimerType = 'N'; // N: No timer, A: Admin, P: Player
 function doTimer(time) {
-  let counter = time;
   timerActive = true;
-  io.emit('update-clock', counter);
+  mainTimer = time
+  io.emit('update-clock', mainTimer);
   let interval = setInterval(() => {
-    counter--;
-    if (counter <= 0 || timerActive == false) {
+    mainTimer--;
+    if (mainTimer <= 0 || timerActive == false) {
       clearInterval(interval);
       io.emit('update-clock', 0);
       this.threeSecTimerType = 'N';
     }
     else {
-      io.emit('update-clock', counter);
+      io.emit('update-clock', mainTimer);
     }
   }, 1000)
 }
@@ -101,7 +102,7 @@ io.on('connection', socket => {
     }
   })
   socket.on('get-match-data', (callback) => {
-    if (callback){
+    if (callback) {
       callback(matchData)
     }
   });
@@ -124,7 +125,222 @@ io.on('connection', socket => {
       fs.writeFileSync(matchDataPath, JSON.stringify(matchData));
       io.emit('update-match-data', matchData);
     }
+  })
+  socket.on('update-data-from-excel', (recievedJSON, callback) => {
+    if (socket.id == adminId){
+      let kdData = JSON.parse(fs.readFileSync(JSON.parse(fs.readFileSync(matchDataPath)).KDFilePath));
+      kdData.questions = [];
+      let vcnvData = JSON.parse(fs.readFileSync(JSON.parse(fs.readFileSync(matchDataPath)).VCNVFilePath));
+      let ttData = JSON.parse(fs.readFileSync(JSON.parse(fs.readFileSync(matchDataPath)).TangTocFilePath));
+      let vdData = JSON.parse(fs.readFileSync(JSON.parse(fs.readFileSync(matchDataPath)).VedichFilePath));
+      vdData.questions = [];
+      console.log('Chuẩn bị dữ liệu lượt 1...');
+      for(let i = 3; i <= 25; i++){
+        if(recievedJSON.kd[i].__EMPTY){
+          let question = {}
+          question.question = recievedJSON.kd[i].__EMPTY;
+          question.answer = recievedJSON.kd[i].__EMPTY_1;
+          if (recievedJSON.kd[i].__EMPTY_2){
+            question.type = 'P'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else if (recievedJSON.kd[i].__EMPTY_3){
+            question.type = 'A'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else {
+            question.type = 'N'
+          }
+          kdData.questions.push(question);
+        }
+      }
+      console.log('Chuẩn bị dữ liệu lượt 2...');
+      for(let i = 30; i <= 54; i++){
+        if(recievedJSON.kd[i].__EMPTY){
+          let question = {}
+          question.question = recievedJSON.kd[i].__EMPTY;
+          question.answer = recievedJSON.kd[i].__EMPTY_1;
+          if (recievedJSON.kd[i].__EMPTY_2){
+            question.type = 'P'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else if (recievedJSON.kd[i].__EMPTY_3){
+            question.type = 'A'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else {
+            question.type = 'N'
+          }
+          kdData.questions.push(question);
+        }
+      }
+      console.log('Chuẩn bị dữ liệu lượt 3...');
+      for(let i = 57; i <= 91; i++){
+        if(recievedJSON.kd[i].__EMPTY){
+          let question = {}
+          question.question = recievedJSON.kd[i].__EMPTY;
+          question.answer = recievedJSON.kd[i].__EMPTY_1;
+          if (recievedJSON.kd[i].__EMPTY_2){
+            question.type = 'P'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else if (recievedJSON.kd[i].__EMPTY_3){
+            question.type = 'A'
+            question.audioFilePath = recievedJSON.kd[i].__EMPTY_2;
+          }
+          else {
+            question.type = 'N'
+          }
+          kdData.questions.push(question);
+        }
+      }
+      console.log(kdData.questions.length)
+      fs.writeFileSync(JSON.parse(fs.readFileSync(matchDataPath)).KDFilePath, JSON.stringify(kdData));
+      io.emit('update-kd-data-admin', kdData);
+      console.log('Hoàn thành nhập đề khởi động');
+      console.log('Bắt đầu nhập đề VCNV...');
+      vcnvData.questions[5].answer = recievedJSON.vcnv[1].__EMPTY;
+      vcnvData.questions[5].picFileName = recievedJSON.vcnv[1].__EMPTY_1;
+      for(let i = 3; i <= 7; i++){
+        vcnvData.questions[i - 3].question = recievedJSON.vcnv[i].__EMPTY;
+        vcnvData.questions[i - 3].answer = recievedJSON.vcnv[i].__EMPTY_1;
+        vcnvData.questions[i - 3].ifOpen = false;
+        if(recievedJSON.vcnv[i].__EMPTY_2){
+          vcnvData.questions[i - 3].audioFilePath = recievedJSON.vcnv[i].__EMPTY_2;
+          vcnvData.questions[i - 3].type = 'HN_S';
+        }
+        else{
+          vcnvData.questions[i - 3].type = 'HN';
+        }
+      }
+      fs.writeFileSync(JSON.parse(fs.readFileSync(matchDataPath)).VCNVFilePath, JSON.stringify(vcnvData));
+      io.emit('update-vcnv-data', vcnvData);
+      console.log('Hoàn thành nhập đề VCNV');
+      console.log('Bắt đầu nhập đề tăng tốc...');
+      for(let i = 2; i <= 5; i++){
+        ttData.questions[i - 2].question = recievedJSON.tt[i].__EMPTY;
+        ttData.questions[i - 2].answer = recievedJSON.tt[i].__EMPTY_1;
+        if(recievedJSON.tt[i].__EMPTY_2){
+          ttData.questions[i - 2].answer_image = recievedJSON.tt[i].__EMPTY_2
+        }
+      }
+      ttData.questions[0].question_image = recievedJSON.tt[10].__EMPTY;
+      ttData.questions[1].question_image = recievedJSON.tt[10].__EMPTY_2;
+      ttData.questions[2].question_image = recievedJSON.tt[10].__EMPTY_4;
+      ttData.questions[3].video_name = recievedJSON.tt[10].__EMPTY_6;
+      fs.writeFileSync(JSON.parse(fs.readFileSync(matchDataPath)).TangTocFilePath, JSON.stringify(ttData));
+      io.emit('update-tangtoc-data', ttData);
+      console.log('Hoàn thành nhập đề tăng tốc');
+      console.log('Bắt đầu nhập đề về đích...');
+      console.log('Chuẩn bị dữ liệu TS 1...');
+      for(let i = 3; i <= 8; i++){
+        vdData.questionPools[0][i - 3].question = recievedJSON.vd[i].__EMPTY;
+        vdData.questionPools[0][i - 3].answer = recievedJSON.vd[i].__EMPTY_1;
+        if(recievedJSON.vd[i]['VỀ ĐÍCH'] == "Câu hỏi 20 điểm"){
+          vdData.questionPools[0][i - 3].value = 20;
+        }
+        else{
+          vdData.questionPools[0][i - 3].value = 30;
+        }
+        if(recievedJSON.vd[i].__EMPTY_2){
+          vdData.questionPools[0][i - 3].type = 'V';
+          vdData.questionPools[0][i - 3].file_name = recievedJSON.vd[i].__EMPTY_2;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_3) {
+          vdData.questionPools[0][i - 3].type = 'I';
+          vdData.questionPools[0][i - 3].file_name = recievedJSON.vd[i].__EMPTY_3;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_5) {
+          vdData.questionPools[0][i - 3].type = 'A';
+          vdData.questionPools[0][i - 3].file_name = recievedJSON.vd[i].__EMPTY_5;
+        }
+        else{
+          vdData.questionPools[0][i - 3].type = 'N';
+        }
+      }
+      console.log('Chuẩn bị dữ liệu TS 2...');
 
+      for(let i = 11; i <= 16; i++){
+        vdData.questionPools[1][i - 11].question = recievedJSON.vd[i].__EMPTY;
+        vdData.questionPools[1][i - 11].answer = recievedJSON.vd[i].__EMPTY_1;
+        if(recievedJSON.vd[i]['VỀ ĐÍCH'] == "Câu hỏi 20 điểm"){
+          vdData.questionPools[1][i - 11].value = 20;
+        }
+        else{
+          vdData.questionPools[1][i - 11].value = 30;
+        }
+        if(recievedJSON.vd[i].__EMPTY_2){
+          vdData.questionPools[1][i - 11].type = 'V';
+          vdData.questionPools[1][i - 11].file_name = recievedJSON.vd[i].__EMPTY_2;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_3) {
+          vdData.questionPools[1][i - 11].type = 'I';
+          vdData.questionPools[1][i - 11].file_name = recievedJSON.vd[i].__EMPTY_3;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_5) {
+          vdData.questionPools[1][i - 11].type = 'A';
+          vdData.questionPools[1][i - 11].file_name = recievedJSON.vd[i].__EMPTY_5;
+        }
+        else{
+          vdData.questionPools[1][i - 11].type = 'N';
+        }
+      }
+      console.log('Chuẩn bị dữ liệu TS 3...');
+      for(let i = 19; i <= 24; i++){
+        vdData.questionPools[2][i - 19].question = recievedJSON.vd[i].__EMPTY;
+        vdData.questionPools[2][i - 19].answer = recievedJSON.vd[i].__EMPTY_1;
+        if(recievedJSON.vd[i]['VỀ ĐÍCH'] == "Câu hỏi 20 điểm"){
+          vdData.questionPools[2][i - 19].value = 20;
+        }
+        else{
+          vdData.questionPools[2][i - 19].value = 30;
+        }
+        if(recievedJSON.vd[i].__EMPTY_2){
+          vdData.questionPools[2][i - 19].type = 'V';
+          vdData.questionPools[2][i - 19].file_name = recievedJSON.vd[i].__EMPTY_2;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_3) {
+          vdData.questionPools[2][i - 19].type = 'I';
+          vdData.questionPools[2][i - 19].file_name = recievedJSON.vd[i].__EMPTY_3;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_5) {
+          vdData.questionPools[2][i - 19].type = 'A';
+          vdData.questionPools[2][i - 19].file_name = recievedJSON.vd[i].__EMPTY_5;
+        }
+        else{
+          vdData.questionPools[2][i - 19].type = 'N';
+        }
+      }
+      console.log('Chuẩn bị dữ liệu TS 4...');
+      for(let i = 27; i <= 32; i++){
+        vdData.questionPools[3][i - 27].question = recievedJSON.vd[i].__EMPTY;
+        vdData.questionPools[3][i - 27].answer = recievedJSON.vd[i].__EMPTY_1;
+        if(recievedJSON.vd[i]['VỀ ĐÍCH'] == "Câu hỏi 20 điểm"){
+          vdData.questionPools[3][i - 27].value = 20;
+        }
+        else{
+          vdData.questionPools[3][i - 27].value = 30;
+        }
+        if(recievedJSON.vd[i].__EMPTY_2){
+          vdData.questionPools[3][i - 27].type = 'V';
+          vdData.questionPools[3][i - 27].file_name = recievedJSON.vd[i].__EMPTY_2;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_3) {
+          vdData.questionPools[3][i - 27].type = 'I';
+          vdData.questionPools[3][i - 27].file_name = recievedJSON.vd[i].__EMPTY_3;
+        }
+        else if(recievedJSON.vd[i].__EMPTY_5) {
+          vdData.questionPools[3][i - 27].type = 'A';
+          vdData.questionPools[3][i - 27].file_name = recievedJSON.vd[i].__EMPTY_5;
+        }
+        else{
+          vdData.questionPools[3][i - 27].type = 'N';
+        }
+      }
+      fs.writeFileSync(JSON.parse(fs.readFileSync(matchDataPath)).VedichFilePath, JSON.stringify(vdData));
+      io.emit('update-vedich-data', vdData);
+      console.log('Hoàn thành nhập từ file excel')
+    }
   })
   socket.on('start-clock', (time) => {
     timerActive = false;
@@ -206,10 +422,13 @@ io.on('connection', socket => {
     socket.broadcast.emit('update-kd-question', '');
   })
   socket.on('get-turn-kd', () => {
-    lastTurnId = socket.id;
-    console.log('sidjfaoisdjfo');
-    io.emit('disable-answer-button-kd');
-    io.emit('player-got-turn-kd', matchData.players[socketIDs.indexOf(socket.id)]);
+    if(lastTurnId == ''){
+      io.emit('play-sfx', 'KD_GET_TURN')
+      threeSecTimerType = 'P';
+      lastTurnId = socket.id;
+      io.emit('disable-answer-button-kd');
+      io.emit('player-got-turn-kd', matchData.players[socketIDs.indexOf(socket.id)]);  
+    }
   })
   socket.on('clear-turn-kd', () => {
     io.emit('clear-turn-player-kd');
@@ -222,15 +441,19 @@ io.on('connection', socket => {
     io.emit('update-match-data', matchData);
     io.to(lastTurnId).emit('update-player-score', matchData.players[socketIDs.indexOf(lastTurnId)].score);
     threeSecTimerType = 'N';
+    lastTurnId = '';
   })
   socket.on('wrong-mark-kd', (ifPlayer) => {
     ifLastAnswerCorrect = false;
-    io.emit('enable-answer-button-kd');
+    if (mainTimer > 0){
+      io.emit('enable-answer-button-kd');
+    }
     if (ifPlayer == true) {
       console.log(lastTurnId)
       io.to(lastTurnId).emit('disable-answer-button-kd');
     }
     threeSecTimerType = 'N';
+    lastTurnId = '';
   })
   socket.on('start-3s-timer-kd', (ifPlayer) => {
     if (ifPlayer) {
@@ -242,26 +465,32 @@ io.on('connection', socket => {
         io.emit('update-3s-timer-kd', counter, true);
         if (threeSecTimerType == 'N') {
           clearInterval(interval);
-          io.emit('enable-answer-button-kd');
+          if (mainTimer > 0){
+            io.emit('enable-answer-button-kd');
+            io.emit('next-question');
+          }
           if (ifLastAnswerCorrect == false) {
             io.to(lastTurnId).emit('disable-answer-button-kd');
             ifLastAnswerCorrect = null;
           }
-          io.emit('clear-turn-player-kd');
+          lastTurnId = '';
           io.emit('update-3s-timer-kd', 0, true)
           io.emit('update-3s-timer-kd', 0, false)
 
         }
         else if (counter == 0) {
           clearInterval(interval);
-          io.emit('enable-answer-button-kd');
+          if (mainTimer > 0){
+            io.emit('enable-answer-button-kd');
+            io.emit('next-question');
+          }
           if (threeSecTimerType == 'P') {
-            io.to(lastTurnId).emit('disable-answer-button-kd');
+            if (mainTimer > 0) io.to(lastTurnId).emit('disable-answer-button-kd');
             threeSecTimerType = 'N';
             io.emit('play-sfx', 'KD_WRONG');
           }
+          lastTurnId = '';
           io.emit('clear-turn-player-kd');
-          io.emit('next-question');
         }
       }, 100);
     }
@@ -273,10 +502,13 @@ io.on('connection', socket => {
         counter--;
         io.emit('update-3s-timer-kd', counter, false);
         if (counter == 0) {
-          io.emit('next-question');
-          io.emit('enable-answer-button-kd');
+          if (mainTimer > 0){
+            io.emit('next-question');
+            io.emit('enable-answer-button-kd');
+          }
           io.emit('clear-turn-player-kd');
           io.emit('play-sfx', 'KD_WRONG');
+          lastTurnId = '';
           clearInterval(interval);
         }
         else if (threeSecTimerType != 'A') {
@@ -577,7 +809,7 @@ io.on('connection', socket => {
       else {
         console.log('Chấm điểm đúng thí sinh ' + id)
         matchData.players[id - 1].score += value;
-        if(vedichData.ifNSHV == false) matchData.players[vedichData.currentPlayerId - 1].score -= value;
+        if (vedichData.ifNSHV == false) matchData.players[vedichData.currentPlayerId - 1].score -= value;
       }
     }
     vedichData.ifNSHV = false;
@@ -597,12 +829,14 @@ io.on('connection', socket => {
     io.emit('update-match-data', matchData);
     io.emit('clear-stealing-player');
   })
-
   socket.on('player-steal-question', () => {
-    io.emit('lock-button-vd');
-    lastStealingPlayerId = socketIDs.indexOf(socket.id);
-    ifFiveSecActive = false;
-    io.emit('player-steal-question', socketIDs.indexOf(socket.id));
+    if (lastStealingPlayerId == -1) {
+      console.log('Cướp câu hỏi');
+      io.emit('lock-button-vd');
+      lastStealingPlayerId = socketIDs.indexOf(socket.id);
+      ifFiveSecActive = false;
+      io.emit('player-steal-question', socketIDs.indexOf(socket.id));
+    }
   })
   socket.on('vd-play-video', () => {
     io.emit('vd-play-video');
@@ -641,5 +875,8 @@ io.on('connection', socket => {
   })
   socket.on('play-sfx', (sfx) => {
     io.emit('play-sfx', sfx);
+  });
+  socket.on('get-chp-data', (callback) => {
+    callback(JSON.parse(fs.readFileSync(JSON.parse(fs.readFileSync(matchDataPath)).ChpFilePath)));
   })
 });
